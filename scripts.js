fd87a9c2651b9dd119bd3325d70bb04e741c4485
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-10 12:10:50 +0530

    JS Added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    
+    clock.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+}
+
+function pad(number) {
+    return number < 10 ? '0' + number : number;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
